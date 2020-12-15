ee65a4a4ba946b130aec5adfa17abf18f495d4d5
Harry Sibbenga <sibbengaharry@gmail.com>
2020-12-15 14:12:24 +0000

    Scrapping worked for each country succsessfully using the for loop

--- solution.py
+++ solution.py
@@ -2,6 +2,7 @@ import requests
 from selenium import webdriver
 import bs4
 import pandas as pd
+import os
 
 url = "http://www.three.co.uk/Support/Roaming_and_international/Roaming_Abroad/Destinations?#countries2"
 
@@ -9,22 +10,29 @@ url = "http://www.three.co.uk/Support/Roaming_and_international/Roaming_Abroad/D
 driver = webdriver.Firefox()
 driver.implicitly_wait(30)
 driver.get(url)
-Xpath = '//ul//li[@class="underline bold"]//a[@title="Brazil."]'
-# python_button = driver.find_element_by_xpath(Xpath)
-python_button = driver.find_element_by_link_text('Brazil.')
-python_button.click()
-
-# res = requests.get('http://www.three.co.uk/support/roaming/brazil')
-
-soup = bs4.BeautifulSoup(driver.page_source, 'lxml')
 
 datalist = []
+query = ["Brazil.", "South Africa.", "Portugal.",
+         "Chile.", "Iceland.", "China.", "Madagascar."]
 
-# get the table
-table = soup.select("table")
-# convert the table into a data frame using pandas
-df = pd.read_html(str(table), header=0)
-# append data to data list
-datalist.append(df[0])
+for country in query:
+    python_button = driver.find_element_by_link_text(country)
+    python_button.click()
 
-print(datalist)
+    soup = bs4.BeautifulSoup(driver.page_source, 'lxml')
+
+    # get the table
+    table = soup.select("table")
+    # convert the table into a data frame using pandas
+    df = pd.read_html(str(table), header=0)
+    # append data to data list
+    datalist.append(df[0])
+
+    driver.execute_script("window.history.go(-1)")
+
+driver.quit()
+
+result = pd.concat([pd.DataFrame(datalist[i])
+                    for i in range(len(datalist))], keys=query)
+
+print(result)
