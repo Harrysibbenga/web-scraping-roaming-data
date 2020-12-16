637bfa4027b7588f579257b03170c702ac645f11
Harry Sibbenga <sibbengaharry@gmail.com>
2020-12-16 16:44:19 +0000

    Created a function for the webdriver and loop functionality, added more comments to the code

--- solution.py
+++ solution.py
@@ -5,32 +5,47 @@ import os
 
 url = "http://www.three.co.uk/Support/Roaming_and_international/Roaming_Abroad/Destinations?#countries2"
 
-# create a new Firefox session
-driver = webdriver.Firefox()
-driver.implicitly_wait(30)
-driver.get(url)
-
-datalist = []
 query = ["Brazil.", "South Africa.", "Portugal.",
          "Chile.", "Iceland.", "China.", "Madagascar."]
 
-for country in query:
-    python_button = driver.find_element_by_link_text(country)
-    python_button.click()
+datalist = []
 
-    soup = bs4.BeautifulSoup(driver.page_source, 'lxml')
 
-    # get the table
-    table = soup.select("table")
-    # convert the table data into a data that will be used in the dataframe with Pandas
-    df = pd.read_html(str(table), header=0)
-    # append data to data list
-    datalist.append(df[0])
+def search_for_countries():
+    '''
+        Runs selenium using firefox webdriver.
+        Loops through the query list above.
+        Gets the page source and uses beautiful soup to get the tables on each page.
+        Uses pandas to read the html and create a df form the data.
+        Store the data in the datalist variable.
+        Quit once looped through all vaiables.
+    '''
+    # create a new Firefox session
+    driver = webdriver.Firefox()
+    driver.implicitly_wait(30)
+    driver.get(url)
 
-    driver.execute_script("window.history.go(-1)")
+    for country in query:
+        python_button = driver.find_element_by_link_text(country)
+        python_button.click()
 
-driver.quit()
+        soup = bs4.BeautifulSoup(driver.page_source, 'lxml')
 
+        # get the table
+        table = soup.select("table")
+        # convert the table data into a data that will be used in the dataframe with Pandas
+        df = pd.read_html(str(table), header=0)
+        # append data to data list
+        datalist.append(df[0])
+
+        driver.execute_script("window.history.go(-1)")
+
+    driver.quit()
+
+
+search_for_countries()
+
+# gets the datalist list and concats to a pandas dataframe
 df_result = pd.concat([pd.DataFrame(datalist[i])
                        for i in range(len(datalist))], keys=query)
 
